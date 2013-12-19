a98aa1441c42ddbac010820b90931790ad151950
DavidSchilling <desideriosemper@gmail.com>
2013-12-19 09:54:01 -0700

    Created the Repository

---
+++ src/io/controller/IOController.java
@@ -0,0 +1,273 @@
+package io.controller;
+
+import io.model.Game;
+import io.view.IOFrame;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class IOController
+{
+	/**
+	 * 
+	 */
+	private IOFrame appFrame;
+
+	/**
+	 * All games that can be retrieved from the save file.
+	 */
+	private ArrayList<Game> projectGames;
+
+	/**
+	 * 
+	 */
+	public IOController()
+	{
+
+	}
+
+	public ArrayList<Game> getProjectGames()
+	{
+		return projectGames;
+	}
+
+	/**
+	 * 
+	 */
+	public void start()
+	{
+		appFrame = new IOFrame(this);
+	}
+
+	/**
+	 * 
+	 * @return: Returns a Game, but it could be null, make sure you check.
+	 */
+	public Game readGameInfo()
+	{
+		String fileName = "saveFile.txt"; // Without a directory, it will look
+											// to the project's directory.
+		File currentSaveFile = new File(fileName);
+
+		/*
+		 * Major Scanner Methods: .next() returns next string separated around
+		 * white space. .nextLine() returns the next line. .nextInt() returns
+		 * the next integer value. .nextBoolean() returns the next boolean
+		 * value. .nextDouble() returns the next double value.
+		 */
+		Scanner fileReader;
+
+		int gameRanking = 0;
+		String gameTitle = "";
+		ArrayList<String> gameRules = new ArrayList<String>();
+		Game currentGame = null;
+
+		try
+		{
+			fileReader = new Scanner(currentSaveFile);
+			gameTitle = fileReader.nextLine();
+			gameRanking = fileReader.nextInt();
+			while (fileReader.hasNext())
+			{
+				gameRules.add(fileReader.nextLine());
+			}
+
+			currentGame = new Game(gameRules, gameRanking, gameTitle);
+			fileReader.close();
+		}
+
+		catch (FileNotFoundException Ralph)
+		{
+			JOptionPane.showMessageDialog(appFrame, "File not found.");
+			JOptionPane.showMessageDialog(appFrame, Ralph.getMessage());
+		}
+
+		return currentGame;
+	}
+
+	/**
+	 * 
+	 * @param toBeParsed
+	 * @return
+	 */
+	private boolean checkNumberFormat(String toBeParsed)
+	{
+		boolean isNumber = false;
+		try
+		{
+			int valid = Integer.parseInt(toBeParsed);
+			isNumber = true;
+		}
+		catch (NumberFormatException Robert)
+		{
+			JOptionPane.showMessageDialog(appFrame, "Not a valid number.");
+		}
+
+		return isNumber;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public Game readGameInformation()
+	{
+		Game currentGame = null;
+		String fileName = "saveFile.txt";
+		File currentSaveFile = new File(fileName);
+		Scanner fileReader;
+		int gameRanking = 0;
+		String gameTitle = "";
+		ArrayList<String> gameRules = new ArrayList<String>();
+
+		try
+		{
+			fileReader = new Scanner(currentSaveFile);
+			gameTitle = fileReader.nextLine();
+			gameRanking = fileReader.nextInt();
+			while (fileReader.hasNext())
+			{
+				gameRules.add(fileReader.nextLine());
+			}
+
+			currentGame = new Game(gameRules, gameRanking, gameTitle);
+			fileReader.close();
+		}
+		catch (FileNotFoundException noFileExists)
+		{
+			JOptionPane.showMessageDialog(appFrame, noFileExists.getMessage());
+		}
+
+		return currentGame;
+	}
+
+	public String readAllGameInformation()
+	{
+		String fileContents = "";
+		String fileName = "saveFile.txt";
+		File currentSaveFile = new File(fileName);
+		Scanner fileReader;
+
+		try
+		{
+			fileReader = new Scanner(currentSaveFile);
+			while (fileReader.hasNext())
+			{
+				fileContents += fileReader.nextLine();
+			}
+			fileReader.close();
+		}
+		catch (FileNotFoundException fileDoesNotExist)
+		{
+			JOptionPane.showMessageDialog(appFrame, fileDoesNotExist.getMessage());
+		}
+
+		return fileContents;
+	}
+
+	/**
+	 * 
+	 * @param currentInfo
+	 */
+	private void convertTextToGames(String currentInfo)
+	{
+		projectGames = new ArrayList<Game>();
+		String[] gameChunks = currentInfo.split(";");
+		for (String currentBlock : gameChunks)
+		{
+			int currentIndex = currentBlock.indexOf("\n");
+			String title = currentBlock.substring(0, currentIndex);
+
+			int nextIndex = currentBlock.indexOf("\n", currentIndex);
+			String ranking = currentBlock.substring(currentIndex + 1, nextIndex);
+			
+			String rules = currentBlock.substring(nextIndex + 1);
+			
+			Game currentGame = makeGameFromInput(title, ranking, rules);
+			
+			projectGames.add(currentGame);
+		}
+	}
+
+	public Game pickRandomGameFromFile()
+	{
+		Game currentGame = null;
+
+		String allInfo = readAllGameInformation();
+
+		return currentGame;
+	}
+
+	/**
+	 * 
+	 * @param title
+	 * @param ranking
+	 * @param rules
+	 * @return
+	 */
+	public Game makeGameFromInput(String title, String ranking, String rules)
+	{
+		Game currentGame = new Game();
+
+		currentGame.setGameTitle(title);
+
+		if (checkNumberFormat(ranking))
+		{
+			currentGame.setFunRanking(Integer.parseInt(ranking));
+		}
+		else
+		{
+			return null;
+		}
+
+		String[] temp = rules.split("\n");
+		ArrayList<String> tempRules = new ArrayList<String>();
+
+		for (String tempWord : temp)
+		{
+			tempRules.add(tempWord);
+		}
+		currentGame.setGameRules(tempRules);
+
+		return currentGame;
+
+	}
+
+	/**
+	 * 
+	 * @param currentGame
+	 */
+	public void saveGameInformation(Game currentGame)
+	{
+		PrintWriter gameWriter;
+		String saveFile = "saveFile.txt";
+
+		try
+		{
+			gameWriter = new PrintWriter(saveFile);
+
+			gameWriter.append(currentGame.getGameTitle());
+			gameWriter.append(Integer.toString(currentGame.getFunRanking()));
+			for (int count = 0; count < currentGame.getGameRules().size(); count++)
+			{
+				gameWriter.append(currentGame.getGameRules().get(count));
+			}
+
+			gameWriter.println(";"); // Allows us to later split the game around
+										// that character.
+
+			gameWriter.close();
+		}
+		catch (FileNotFoundException noFileExists)
+		{
+			JOptionPane.showMessageDialog(appFrame, "Could not Create the save file.");
+			JOptionPane.showMessageDialog(appFrame, noFileExists.getMessage());
+		}
+	}
+
+}
---
+++ src/io/view/IOPanel.java
@@ -0,0 +1,136 @@
+package io.view;
+
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.*;
+
+import io.controller.IOController;
+import io.model.Game;
+
+@SuppressWarnings("serial")
+public class IOPanel extends JPanel
+{
+	private JButton saveButton;
+	private JButton loadButton;
+	private JTextField titleField;
+	private JLabel titleLabel;
+	private JTextField rankingField;
+	private JLabel rankingLabel;
+	private JTextArea rulesArea;
+	private JLabel rulesLabel;
+	private SpringLayout baseLayout;
+	private IOController baseController;
+	private JLabel gameCountLabel;
+
+	public IOPanel(IOController baseController)
+	{
+		this.baseController = baseController;
+
+		saveButton = new JButton("SAVE");
+		loadButton = new JButton("LOAD");
+		titleField = new JTextField(15);
+		titleLabel = new JLabel("GameTitle: ");
+		rankingField = new JTextField(5);
+		rankingLabel = new JLabel("Rank: ");
+		rulesArea = new JTextArea(5, 20);
+		rulesLabel = new JLabel("Game Rules: ");
+		baseLayout = new SpringLayout();
+
+		gameCountLabel = new JLabel("Number of Games: ");
+
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+
+	private void setupPanel()
+	{
+		this.setLayout(baseLayout);
+		this.setBackground(Color.lightGray);
+		this.add(rankingField);
+		this.add(rankingLabel);
+		this.add(rulesArea);
+		this.add(rulesLabel);
+		this.add(saveButton);
+		this.add(titleField);
+		this.add(titleLabel);
+		this.add(gameCountLabel);
+		this.add(loadButton);
+
+	}
+
+	private void setupLayout()
+	{
+		baseLayout.putConstraint(SpringLayout.NORTH, loadButton, 20, SpringLayout.SOUTH, rulesArea);
+		baseLayout.putConstraint(SpringLayout.NORTH, saveButton, 0, SpringLayout.NORTH, loadButton);
+		baseLayout.putConstraint(SpringLayout.EAST, loadButton, 0, SpringLayout.EAST, titleField);
+		baseLayout.putConstraint(SpringLayout.NORTH, rulesArea, -5, SpringLayout.NORTH, rulesLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, rulesArea, 0, SpringLayout.WEST, rankingField);
+		baseLayout.putConstraint(SpringLayout.WEST, rankingField, 35, SpringLayout.EAST, rankingLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, rulesLabel, 23, SpringLayout.SOUTH, rankingLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, rulesLabel, 0, SpringLayout.WEST, rankingLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, titleField, 0, SpringLayout.WEST, rankingField);
+		baseLayout.putConstraint(SpringLayout.SOUTH, titleField, -6, SpringLayout.NORTH, rankingField);
+		baseLayout.putConstraint(SpringLayout.NORTH, rankingField, -3, SpringLayout.NORTH, rankingLabel);
+		baseLayout.putConstraint(SpringLayout.NORTH, rankingLabel, 14, SpringLayout.SOUTH, titleLabel);
+		baseLayout.putConstraint(SpringLayout.SOUTH, titleLabel, -228, SpringLayout.SOUTH, this);
+		baseLayout.putConstraint(SpringLayout.WEST, rankingLabel, 0, SpringLayout.WEST, titleLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, titleLabel, 68, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.EAST, saveButton, 0, SpringLayout.EAST, gameCountLabel);
+		baseLayout.putConstraint(SpringLayout.WEST, gameCountLabel, 68, SpringLayout.WEST, this);
+		baseLayout.putConstraint(SpringLayout.SOUTH, gameCountLabel, -271, SpringLayout.SOUTH, this);
+
+	}
+
+	private void setupListeners()
+	{
+
+		saveButton.addActionListener(new ActionListener()
+		{
+			@Override
+			public void actionPerformed(ActionEvent click)
+			{
+				Game tempGame = baseController.makeGameFromInput(titleField.getText(), rankingField.getText(),
+						rulesArea.getText());
+				if (tempGame != null)
+				{
+					baseController.saveGameInformation(tempGame);
+					gameCountLabel.setText("Number of Games: " + baseController.getProjectGames().size());
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Not a valid number");
+				}
+			}
+
+		});
+
+		loadButton.addActionListener(new ActionListener()
+		{
+			public void actionPerformed(ActionEvent click)
+			{
+				Game tempGame = baseController.readGameInformation();
+				if (tempGame != null)
+				{
+					titleField.setText(tempGame.getGameTitle());
+					rankingField.setText(Integer.toString((tempGame.getFunRanking())));
+					String tempRules = "";
+					for (String currentRule : tempGame.getGameRules())
+					{
+						tempRules += currentRule + "\n";
+					}
+
+				}
+				else
+				{
+					JOptionPane.showMessageDialog(null, "Check the file, make sure it is in order.");
+				}
+
+			}
+
+		});
+
+	}
+}
